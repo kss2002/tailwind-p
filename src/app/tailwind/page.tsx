0d691d920ab01a6ba6b4264388526e6caf3814c7
seong su kim <know.warehouse02@gmail.com>
2025-07-01 19:20:12 +0900

    page setting

--- src/app/tailwind/page.tsx
+++ src/app/tailwind/page.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
+import styles from "../tailwind/main.module.css";
 
 export default function tailwind() {
   return (
     <>
-      <h3>tailwind css</h3>
-      <div>page</div>
-      <button>
-        <Link href="/" rel="noopener noreferrer">
-          <span>Go to Home</span>
-        </Link>
-      </button>
+      <main>
+        <p className={styles.main__tilte}>tailwind css</p>
+        <button className={styles.main__button}>
+          <Link href="/" rel="noopener noreferrer">
+            <span>Go to Home</span>
+          </Link>
+        </button>
+      </main>
+
+      <section>
+        <div>
+          <p className={styles.main__desc}>
+            tailwind css의 모든 것을 다루지는 않고, 자주 쓰이는 유틸리티만 다룰
+            예정
+          </p>
+        </div>
+      </section>
     </>
   );
 }
