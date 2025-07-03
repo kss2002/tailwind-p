c95c89d1390b104f021642af872cecc1003cc202
seong su kim <know.warehouse02@gmail.com>
2025-07-03 19:10:33 +0900

    page setting

--- src/app/shad/page.tsx
+++ src/app/shad/page.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
+import styles from "../shad/main.module.css";
 
 export default function shad() {
   return (
     <>
-      <h3>shadcn ui</h3>
-      <div>page</div>
-      <button>
-        <Link href="/" rel="noopener noreferrer">
-          <span>Go to Home</span>
-        </Link>
-      </button>
+      <main>
+        <p className={styles.main__tilte}>shadcn ui</p>
+        <button className={styles.main__button}>
+          <Link href="/" rel="noopener noreferrer">
+            <span>Go to Home</span>
+          </Link>
+        </button>
+      </main>
+
+      <section>
+        <div>
+          <p className={styles.main__desc}>
+            shadcn ui 는 컴포넌트 라이브러리로, Tailwind CSS를 기반으로
+            만들어짐.
+          </p>
+        </div>
+      </section>
     </>
   );
 }
