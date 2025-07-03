4843aab72c44667b66023ec22f1512c627c8d9fd
seong su kim <know.warehouse02@gmail.com>
2025-07-03 19:10:03 +0900

    page setting

--- src/app/magic/page.tsx
+++ src/app/magic/page.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
+import styles from "../magic/main.module.css";
 
 export default function magic() {
   return (
     <>
-      <h3>magic ui</h3>
-      <div>page</div>
-      <button>
-        <Link href="/" rel="noopener noreferrer">
-          <span>Go to Home</span>
-        </Link>
-      </button>
+      <main>
+        <p className={styles.main__tilte}>magic ui</p>
+        <button className={styles.main__button}>
+          <Link href="/" rel="noopener noreferrer">
+            <span>Go to Home</span>
+          </Link>
+        </button>
+      </main>
+
+      <section>
+        <div>
+          <p className={styles.main__desc}>
+            magic ui 는 컴포넌트 라이브러리로, Tailwind CSS를 기반으로 만들어짐.
+            (애니메이션에 특화됨)
+          </p>
+        </div>
+      </section>
     </>
   );
 }
