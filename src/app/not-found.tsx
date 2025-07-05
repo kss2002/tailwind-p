8ea174004a20ce5f3161a17edfc4168578b0c7dd
seong su kim <know.warehouse02@gmail.com>
2025-07-05 15:45:44 +0900

    not-found styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import styles from "./tailwind/main.module.css";
 
 // app 하위 파일에 not-found로 파일을 명하면,
 // 자동으로 404 not found 페이지를 보여줌
 export default function NotFound() {
   return (
     <>
-      <h1>찾을 수 없는 페이지입니다.</h1>
+      <h1 className={styles.main__tilte}>찾을 수 없는 페이지입니다.</h1>
       <div>
-        <button>
+        <button className={styles.main__button}>
           <Link href="/">Home</Link>
         </button>
       </div>
